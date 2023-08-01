6a96e70c63aade6dce5e1135f8d1670d354f1fad
강민 <103026813+alsdl0629@users.noreply.github.com>
2023-08-01 23:28:52 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.c
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.c
@@ -1,37 +1,13 @@
 #include <stdio.h>
+int arr[10];
 int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-
-    long long int int_result = a * b * c;
-    char string_result[10];
-    sprintf(string_result, "%lld", int_result);
-    
-    int count[10] = {0};
-    for (int i = 0; i < strlen(string_result); i ++) {
-        if (string_result[i] == '0') {
-            count[0]++;
-        } else if (string_result[i] == '1') {
-            count[1]++;
-        } else if (string_result[i] == '2') {
-            count[2]++;
-        } else if (string_result[i] == '3') {
-            count[3]++;
-        } else if (string_result[i] == '4') {
-            count[4]++;
-        } else if (string_result[i] == '5') {
-            count[5]++;
-        } else if (string_result[i] == '6') {
-            count[6]++;
-        } else if (string_result[i] == '7') {
-            count[7]++;
-        } else if (string_result[i] == '8') {
-            count[8]++;
-        } else if (string_result[i] == '9') {
-            count[9]++;
-        }
+    int result = a * b * c;
+    for (int i = result; i > 0; i /= 10) {
+        arr[i % 10]++;
     }
-    for (int i = 0; i < 10; i ++) {
-        printf("%d\n", count[i]);
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", arr[i]);
     }
 }
