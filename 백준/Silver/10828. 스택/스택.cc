ec456bebc08a6ea9434392c5ac118065d0f7af50
강민 <103026813+alsdl0629@users.noreply.github.com>
2023-09-10 23:45:04 +0900

    [Silver IV] Title: 스택, Time: 360 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+    stack<int> S;
+    int n;
+    cin >> n;
+    while (n--) {
+        string str;
+        cin >> str;
+        if (str == "push") {
+            int input;
+            cin >> input;
+            S.push(input);
+        } else if (str == "top") {
+            if (!S.empty()) cout << S.top() << "\n";
+            else cout << -1 << "\n";
+        } else if (str == "size") {
+            cout << S.size() << "\n";
+        } else if (str == "empty") {
+            if (S.empty()) cout << 1 << "\n";
+            else cout << 0 << "\n";
+        } else if (str == "pop") {
+            if (!S.empty()) {
+                cout << S.top() << "\n";
+                S.pop();
+            } else cout << -1 << "\n";
+        }
+    }
+}
