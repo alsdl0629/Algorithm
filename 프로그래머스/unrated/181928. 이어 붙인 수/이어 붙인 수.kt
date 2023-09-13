ba59b2fb20a18e156f192d28b201de1784a83faa
강민 <103026813+alsdl0629@users.noreply.github.com>
2023-09-13 19:03:36 +0900

    [unrated] Title: 이어 붙인 수, Time: 28.24 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.kt
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.kt
@@ -1,24 +1,3 @@
 class Solution {
-    fun solution(num_list: IntArray): Int {
-        var answer: Int = 0
-        var a = 0
-        var b = 0
-        num_list.forEach { i ->
-            if (i % 2 == 0) {
-                if (i == 0) {
-                    a = i
-                } else {
-                    a = a * 10 + i
-                }
-            } else {
-                if (i == 0) {
-                    b = i
-                } else {
-                    b = b * 10 + i
-                }
-            }
-        }
-        answer = a + b
-        return answer
-    }
+    fun solution(num_list: IntArray): Int = num_list.partition { it % 2 == 0 }.toList().sumOf { it.joinToString("").toInt() }
 }
