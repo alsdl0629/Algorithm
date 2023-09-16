0ea72c09c4978357742596f1442be6108dfd176b
강민 <103026813+alsdl0629@users.noreply.github.com>
2023-09-16 22:33:05 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.28 ms, Memory: 60.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.kt
@@ -0,0 +1,16 @@
+import kotlin.math.pow
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        if (n % 2 != 0) {
+            for (i in 1..n step(2)) {
+                answer += i
+            }
+        } else {
+            for (i in 2 .. n step(2)) {
+                answer += i.toDouble().pow(2).toInt()
+            }
+        }
+        return answer
+    }
+}
