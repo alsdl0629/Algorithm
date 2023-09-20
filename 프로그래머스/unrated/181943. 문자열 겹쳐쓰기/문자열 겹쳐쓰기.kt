d1f2584bb289232e0007bb4b23f0062e2537b7fa
강민 <103026813+alsdl0629@users.noreply.github.com>
2023-09-20 19:09:37 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 17.37 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
@@ -1,6 +1,16 @@
 class Solution {
     fun solution(my_string: String, overwrite_string: String, s: Int): String {
-        var answer: String = ""
-        return answer
+         var answer: String = ""
+    var str = ""
+    if (overwrite_string.length + 1 <= my_string.length - s) { // my_stirng 이 더 길면
+        str = my_string.substring(0, s)
+        var end = my_string.substring(s + overwrite_string.length)
+        println(str)
+        answer = str + overwrite_string + end
+    } else {
+        str = my_string.substring(0, s)
+        answer = str + overwrite_string
+    }
+    return answer
     }
 }
