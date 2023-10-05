ff54ce7b2adacbe5bf84f1a476ce1815d115013e
강민 <ehfehf0629@gmail.com>
2023-10-05 19:59:14 +0900

    [unrated] Title: 배열의 원소만큼 추가하기, Time: 9.13 ms, Memory: 62.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        for (i in arr.indices) for (j in 1..arr[i]) answer += arr[i]
+        return answer
+    }
+}
