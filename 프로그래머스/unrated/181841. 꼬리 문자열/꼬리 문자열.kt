41eae7496bf07691eb0fd4dfa82cae5e84133564
강민 <ehfehf0629@gmail.com>
2023-10-05 20:02:35 +0900

    [unrated] Title: 꼬리 문자열, Time: 9.69 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(str_list: Array<String>, ex: String): String {
+        var answer: String = ""
+        str_list.forEach { if (!it.contains(ex)) answer += it }
+        return answer
+    }
+}
