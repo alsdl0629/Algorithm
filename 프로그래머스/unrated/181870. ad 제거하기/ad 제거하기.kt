ee9f3539155e7eaf058f70066475f46cbb97f877
강민 <ehfehf0629@gmail.com>
2023-10-08 14:34:04 +0900

    [unrated] Title: ad 제거하기, Time: 7.03 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.kt
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(strArr: Array<String>): Array<String> =  strArr.filterNot { it.contains("ad".toRegex()) }.toTypedArray()
+    fun solution(strArr: Array<String>): Array<String> =  strArr.filterNot { it.contains("ad") }.toTypedArray()
 }
