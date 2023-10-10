c669428d28d12bb229f2314c0bebd17234e57a64
강민 <ehfehf0629@gmail.com>
2023-10-10 19:12:05 +0900

    [level 2] Title: 기능개발, Time: 16.76 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.kt
+++ 프로그래머스/2/42586. 기능개발/기능개발.kt
@@ -1,29 +1,29 @@
 class Solution {
     fun solution(progresses: IntArray, speeds: IntArray): IntArray {
         var answer = intArrayOf()
-    val DQ = ArrayDeque<Int>()
-    for (i in progresses.indices) {
-        var day = 0
-        var sum = progresses[i]
-        while (true) {
-            sum += speeds[i]
-            day++
-            if (sum >= 100) break
-        }
-        DQ.addLast(day)
-    }
-    while (DQ.isNotEmpty()) {
-        var removal = DQ.removeFirst()
-        var count = 1
-        while (DQ.isNotEmpty()) {
-            if (removal >= DQ.first()) {
-                count++
-                DQ.removeFirst()
+        val DQ = ArrayDeque<Int>()
+        for (i in progresses.indices) {
+            var day = 0
+            var sum = progresses[i]
+            while (true) {
+                sum += speeds[i]
+                day++
+                if (sum >= 100) break
             }
-            else break
+            DQ.addLast(day)
         }
-        answer += count
-    }
-    return answer
+        while (DQ.isNotEmpty()) {
+            var removal = DQ.removeFirst()
+            var count = 1
+            while (DQ.isNotEmpty()) {
+                if (removal >= DQ.first()) {
+                    count++
+                    DQ.removeFirst()
+                }
+                else break
+            }
+            answer += count
+        }
+        return answer
     }
 }
