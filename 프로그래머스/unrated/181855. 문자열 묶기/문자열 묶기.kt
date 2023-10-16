c03c96a4e55a0dd0ad938acf7e402b83e20295a3
강민 <ehfehf0629@gmail.com>
2023-10-16 10:20:41 +0900

    [level unrated] Title: 문자열 묶기, Time: 30.64 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.kt
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.kt
@@ -1,17 +1,7 @@
 class Solution {
     fun solution(strArr: Array<String>): Int {
         var store = IntArray (100000) { 0 }
-    strArr.map { store[it.length]++ }
-//    var max = -999
-//    for (i in store.indices) {
-//        if (store[i] > max) {
-//            max = i
-//        }
-//    }
-//    store.forEach {
-//        println(it)
-//    }
-//    println(max)
-    return store.maxOf { it }
+        strArr.map { store[it.length]++ }
+        return store.maxOf { it }
     }
 }
