0c7be1dac4866ae496965dec8da238f353da1651
강민 <ehfehf0629@gmail.com>
2023-11-13 20:00:48 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
@@ -1,13 +1,11 @@
 import kotlin.math.max
 class Solution {
     fun solution(n: Int, m: Int): IntArray {
-        var answer = IntArray(2)
-        for (i in 1..max(n, m)) {
-            if (n % i == 0 && m % i == 0) answer[0] = i
-        }
-        var a = n / answer[0]
-        var b = m / answer[0]
-        answer[1] = answer[0] * a * b
-        return answer
+        var gcm = gcm(n, m)
+        return intArrayOf(gcm, (n * m) / gcm)
+    }
+
+    tailrec fun gcm(a: Int, b: Int): Int {
+        return if (b == 0) a else gcm(b, a % b)
     }
 }
