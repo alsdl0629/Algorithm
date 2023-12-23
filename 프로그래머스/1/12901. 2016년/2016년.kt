fe633017a56901105b45660404bfd6e99f6df280
강민 <ehfehf0629@gmail.com>
2023-12-23 18:13:10 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun solution(a: Int, b: Int) = when (a) {
+    1 -> day(b)
+    2 -> day(31 + b)
+    3 -> day(60 + b)
+    4 -> day(91 + b)
+    5 -> day(121 + b)
+    6 -> day(152 + b)
+    7 -> day(182 + b)
+    8 -> day(213 + b)
+    9 -> day(244 + b)
+    10 -> day(274 + b)
+    11 -> day(305 + b)
+    else -> day(335 + b)
+}
+
+
+private fun day(n: Int): String {
+    val i = (n - 1) % 7
+    return when (i) {
+        0 -> "FRI"
+        1 -> "SAT"
+        2 -> "SUN"
+        3 -> "MON"
+        4 -> "TUE"
+        5 -> "WED"
+        else -> "THU"
+    }
+}
+}
