a1ea80389f9f3d382e54a880d899bf32e638148f
강민 <ehfehf0629@gmail.com>
2023-12-25 14:51:46 +0900

    [level 1] Title: 카드 뭉치, Time: 16.17 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
@@ -1,6 +1,5 @@
 class Solution {
     fun solution(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
-        var result = true
         val DQ1 = ArrayDeque<String>()
         val DQ2 = ArrayDeque<String>()
         cards1.map { DQ1.add(it) }
@@ -13,9 +12,9 @@ class Solution {
             } else if (DQ2.isNotEmpty() && i == DQ2.first()) {
                 DQ2.removeFirst()
                 continue
-            } else result = false
+            } else return "No"
         }
 
-        return if (result) "Yes" else "No"
+        return "Yes"
     }
 }
