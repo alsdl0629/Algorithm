3d25b39c6dfd855458217f944b88d8e74ac56d9e
강민 <ehfehf0629@gmail.com>
2024-04-09 17:16:31 +0900

    [level 3] Title: 단어 변환, Time: 17.00 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.kt
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.kt
@@ -4,31 +4,31 @@ class Solution {
 
     fun solution(begin: String, target: String, words: Array<String>): Int {
         if (!words.contains(target)) return 0
-        visited = BooleanArray(words.size) {false}
+        visited = BooleanArray(words.size)
+
         dfs(begin, target, words, 0)
         return result
     }
 
-    private fun dfs(begin: String, target: String, words: Array<String>, step: Int) {
-        if (step >= result) return
-        else if (begin == target) result = minOf(result, step)
+    fun dfs(begin: String, target: String, words: Array<String>, cnt: Int) {
+        if (cnt >= result) return
+        else if (begin == target) result = minOf(result, cnt)
         else {
             for (i in words.indices) {
                 if (!visited[i] && checkOneLetterDiff(begin, words[i])) {
                     visited[i] = true
-                    dfs(words[i], target, words, step + 1)
+                    dfs(words[i], target, words, cnt + 1)
                     visited[i] = false
                 }
             }
         }
     }
 
-    private fun checkOneLetterDiff(first: String, second: String): Boolean {
-        var count = 0
-        first.forEachIndexed { index, _ ->
-            if (first[index] != second[index]) count++
+    private fun checkOneLetterDiff(begin: String, target: String): Boolean {
+        var cnt = 0
+        begin.forEachIndexed { index, _ ->
+            if (begin[index] !=target[index]) cnt++
         }
-
-        return count == 1
+        return cnt == 1
     }
 }
