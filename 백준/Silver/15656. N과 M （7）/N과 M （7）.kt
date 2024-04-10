4fc18fa0a55ab21f96541aab08de46977eca8f94
강민 <ehfehf0629@gmail.com>
2024-04-10 18:30:20 +0900

    [Silver III] Title: N과 M (7), Time: 740 ms, Memory: 270860 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.kt
@@ -0,0 +1,26 @@
+private val sb = StringBuilder()
+private var n = 0
+private var m = 0
+private lateinit var numbers: IntArray
+private lateinit var tmp: IntArray
+fun main() = with(System.`in`.bufferedReader()) {
+    readLine().split(" ").apply {
+        n = this[0].toInt()
+        m = this[1].toInt()
+    }
+    numbers = readLine().split(" ").map { it.toInt() }.sorted().toIntArray()
+    tmp = IntArray(m)
+    dfs(0)
+    println(sb)
+}
+fun dfs(depth: Int) {
+    if (depth == m) {
+        tmp.forEach { sb.append("$it ") }
+        sb.append("\n")
+    } else {
+        for (i in numbers.indices) {
+            tmp[depth] = numbers[i]
+            dfs(depth + 1)
+        }
+    }
+}
