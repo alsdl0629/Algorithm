5177240ca4c94b2c7c92f3e51e80cbce22f80e9b
강민 <ehfehf0629@gmail.com>
2024-04-11 08:37:14 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,6 +1,6 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 11일 08:36:43
+2024년 04월 11일 08:37:13
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
@@ -1,13 +1,43 @@
+import java.util.LinkedList
 class Solution {
     fun solution(n: Int, a: Int, b: Int): Int {
-        var answer = 0
-        var na = a
-        var nb = b
-        while (na != nb) {
-            na = (na + 1) / 2
-            nb = (nb + 1) / 2
+        var answer = 1
+        val numbers = LinkedList<Int>()
+        repeat(n) { numbers += it + 1 }
+        loop@while (true) {
+            var idx = 0
+            if (numbers.size == 2) break@loop
+            else {
+                while (idx != numbers.size) {
+                    if (numbers[idx] == a && numbers[idx + 1] == b
+                        || numbers[idx] == b && numbers[idx + 1] == a) break@loop
+                    idx += 2
+                }
+            }
+
+            idx = 0
+            while (numbers.size != idx) {
+                if (numbers[idx] != a && numbers[idx] != b && numbers[idx + 1]  != a && numbers[idx + 1] != b) {
+                    numbers.removeAt(idx)
+                } // 해당 조에는 a, b가 존재하지 않음
+                else if (numbers[idx] == a && numbers[idx] != b && numbers[idx + 1] != a && numbers[idx + 1] != b) {
+                    numbers.removeAt(idx + 1)
+                } // 앞에가 a일 때
+                else if (numbers[idx] != a && numbers[idx] != b && numbers[idx + 1] == a && numbers[idx + 1] != b) {
+                    numbers.removeAt(idx)
+                } // 뒤가 a일 때
+                else if (numbers[idx] != a && numbers[idx] == b && numbers[idx + 1] != a && numbers[idx + 1] != b) {
+                    numbers.removeAt(idx + 1)
+                } // 앞에가 b일 때
+                else if (numbers[idx] != a && numbers[idx] != b && numbers[idx + 1] != a && numbers[idx + 1] == b) {
+                    numbers.removeAt(idx)
+                } // 앞에가 b일 때
+                idx += 1
+            }
+
             answer++
         }
+
         return answer
     }
 }
