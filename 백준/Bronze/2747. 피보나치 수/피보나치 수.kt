732d2444e970d575e6129d7b1d551da71bd2306f
강민 <ehfehf0629@gmail.com>
2024-04-13 22:12:00 +0900

    [Bronze II] Title: 피보나치 수, Time: 88 ms, Memory: 12244 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.kt
@@ -0,0 +1,11 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    if (n <= 2) println(1)
+    else {
+        val arr = IntArray(n + 1)
+        arr[1] = 1
+        arr[2] = 1
+        for (i in 3 .. n) arr[i] = arr[i - 1] + arr[i - 2]
+        println(arr[n])
+    }
+}
