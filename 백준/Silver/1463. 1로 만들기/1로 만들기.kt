0c4707dd209844c747f80418fd9fcafcae05a577
강민 <ehfehf0629@gmail.com>
2024-04-13 22:40:34 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 16568 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.kt
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.kt
@@ -1,17 +1,10 @@
-import kotlin.math.min
-
-fun problem(n: Int): Int {
-    var dp = IntArray(n + 1) { 0 } // 0 ~ 10
-    for (i in 2..n) { // 2 ~ 10
-        dp[i] = dp[i - 1] + 1 // 1 + 1
-        if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1)
-        if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1)
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val dp = IntArray(n + 1)
+    for (i in 2..n) {
+        dp[i] = dp[i - 1] + 1
+        if (i % 3 == 0) dp[i] = minOf(dp[i], dp[i / 3] + 1)
+        if (i % 2 == 0) dp[i] = minOf(dp[i], dp[i / 2] + 1)
     }
-    return dp[n]
-}
-
-fun main() {
-    val readln = readln()
-    val problem = problem(readln.toInt())
-    println(problem)
+    println(dp[n])
 }
