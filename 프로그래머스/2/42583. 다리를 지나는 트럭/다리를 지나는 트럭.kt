a55a2f98c5cc86147e9b169963f100bab41cf6c1
강민 <ehfehf0629@gmail.com>
2024-04-13 20:37:38 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 34.24 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
@@ -1,29 +1,21 @@
 class Solution {
-    fun solution(bridgeLength: Int, weight: Int, truckWeights: IntArray): Int {
+    fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
         var time = 0
-
         val trucks = ArrayDeque<Int>()
-        truckWeights.forEach {
-            trucks.addFirst(it)
+        truck_weights.forEach { trucks.addFirst(it) }
+
+        val moving = ArrayDeque<Int>(bridge_length).apply {
+            for (i in 1..bridge_length) this.addFirst(0)
         }
 
-        val bridge = ArrayDeque<Int>(bridgeLength)
-        for (i in 1..bridgeLength) {
-            bridge.addFirst(0)
-        }
-
-        var currentWeight = 0
         while (trucks.isNotEmpty()) {
+            val last = trucks.last()
+            moving.removeLast()
+            if (moving.sum() + last <= weight) moving.addFirst(trucks.removeLast())
+            else moving.addFirst(0)
+
             time++
-            currentWeight -= bridge.removeLast()
-
-            if (currentWeight + trucks.last() <= weight) {
-                currentWeight += trucks.last()
-                bridge.addFirst(trucks.removeLast())
-            } else bridge.addFirst(0)
-            
         }
-
-        return time + bridgeLength
+        return time + bridge_length
     }
 }
