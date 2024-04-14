c6dc339a3987ecd98616f45efa9af0fe2eab0fc6
강민 <ehfehf0629@gmail.com>
2024-04-14 23:05:34 +0900

    [level 3] Title: 단어 변환, Time: 13.59 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.kt
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.kt
@@ -1,34 +1,31 @@
 class Solution {
-    private lateinit var visited: BooleanArray
     private var result = 50
+    private lateinit var visited: BooleanArray
 
     fun solution(begin: String, target: String, words: Array<String>): Int {
         if (!words.contains(target)) return 0
-        visited = BooleanArray(words.size) {false}
-        dfs(begin, target, words, 0)
+        visited = BooleanArray(words.size)
+        dfs(begin, target, words, 0, )
         return result
     }
 
-    private fun dfs(begin: String, target: String, words: Array<String>, step: Int) {
-        if (step >= result) return
-        else if (begin == target) result = minOf(result, step)
+    fun dfs(begin: String, target: String, words: Array<String>, count: Int) {
+        if (count >= result) return
+        else if (begin == target) result = minOf(result, count)
         else {
             for (i in words.indices) {
-                if (!visited[i] && checkOneLetterDiff(begin, words[i])) {
+                if (!visited[i] && checkOneDiffLetter(begin, words[i])) {
                     visited[i] = true
-                    dfs(words[i], target, words, step + 1)
+                    dfs(words[i], target, words, count + 1)
                     visited[i] = false
                 }
             }
         }
     }
 
-    private fun checkOneLetterDiff(first: String, second: String): Boolean {
-        var count = 0
-        first.forEachIndexed { index, _ ->
-            if (first[index] != second[index]) count++
-        }
-
-        return count == 1
+    private fun checkOneDiffLetter(a: String, b: String): Boolean {
+        var diffCount = 0
+        a.forEachIndexed { index, _ -> if (a[index] != b[index]) diffCount++ }
+        return diffCount == 1
     }
 }
