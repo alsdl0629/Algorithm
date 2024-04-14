442558ee026cb980e9c7cbb2a93ad1c548101bc6
강민 <ehfehf0629@gmail.com>
2024-04-14 14:02:52 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 12432 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.kt
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.kt
@@ -1,18 +1,14 @@
-import kotlin.math.max
-
-fun main() {
-    var readln = readln().toInt() // 1
-    var arr = IntArray(readln) { 0 } // 1
-    for (i in 0 until readln) arr[i] = readln().toInt()
-    if (readln == 1) {
-        println(arr[0])
-    } else {
-        var dp = IntArray(readln + 1) { 0 } // 2
-        dp[1] = arr[0]
-        dp[2] = max(dp[1] + arr[1], arr[1])
-        for (i in 3..readln) { // 3 ~ 6
-            dp[i] = max(dp[i - 2] + arr[i - 1], dp[i - 3] + arr[i - 2] + arr[i - 1])
-        }
-        println(dp[dp.lastIndex])
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val stair = IntArray(301)
+    for (i in 1..n) stair[i] = readLine().toInt()
+    val dp = IntArray(301)
+    dp[1] = stair[1]
+    dp[2] = stair[1] + stair[2]
+    dp[3] = maxOf(stair[1] + stair[3], stair[2] + stair[3])
+    for (i in 4..n) {
+        val current = stair[i]
+        dp[i] = maxOf(dp[i - 3] + stair[i - 1] + current, dp[i - 2] + current)
     }
+    println(dp[n])
 }
