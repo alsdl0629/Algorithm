3a53bcf8ca224a4073255ca55478536eca55f717
강민 <ehfehf0629@gmail.com>
2024-04-14 14:48:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 88 ms, Memory: 12308 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.kt
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.kt
@@ -1,16 +1,13 @@
-fun main() {
-    val readln = readln().toInt()
-    var dp = IntArray(readln + 1) { 0 }
-    if (readln == 1) {
-        println(1)
-    } else if (readln == 2) {
-        println(2)
-    } else {
-        dp[1] = 1
-        dp[2] = 2
-        for (i in 3..readln) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+fun main() = with(System.`in`.bufferedReader()) {
+    when (val n = readLine().toInt()) {
+        1 -> println(1)
+        2 -> println(2)
+        else -> {
+            val dp = IntArray(n + 1)
+            dp[1] = 1
+            dp[2] = 2
+            for (i in 3..n) dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+            println(dp[n])
         }
-        println(dp[readln])
     }
 }
