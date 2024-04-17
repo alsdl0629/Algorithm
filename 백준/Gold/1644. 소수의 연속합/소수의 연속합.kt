0b22f9b2f18d89d4f3bb8b172fae044ed0352d7f
강민 <ehfehf0629@gmail.com>
2024-04-17 14:50:55 +0900

    [Gold III] Title: 소수의 연속합, Time: 1548 ms, Memory: 22340 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.kt
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.kt
@@ -5,22 +5,24 @@ import kotlin.math.sqrt
 fun main() = with(System.`in`.bufferedReader()) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = readLine().toInt()
-    var end = 2
+    var start = 0
+    var end = 0
     var total = 0
     var count = 0
-
-    for (start in 2 .. n) {
-        if (isPrime(start)) {
-            while (n > total) {
-                if (isPrime(end)) {
-                    total += end
-                    end++
-                } else end++
-            }
-            if (n == total) count++
-            total -= start
-        }
+    val arr = mutableListOf<Int>()
+    for (i in 2..n) {
+        if (isPrime(i)) arr += i
     }
+
+    val size = arr.size
+    while (true) {
+        if (total >= n) { // 누적합이 n보다 클 때
+            if (total == n) count++ // 누적합이 n과 같을 때
+            total -= arr[start++]
+        } else if (end == size) break
+        else total += arr[end++] // 누적합이 n보다 작을 때
+    }
+
     bw.let {
         it.write("$count")
         it.flush()
